aa23552fe866da47c3797cb2f7b1099660a3ff4d
Kincent <kinlan19@bergen.org>
2020-08-19 14:11:51 -0400

    Updates

---
+++ precache-manifest.fe119f2428233ae62b289d9bd721acc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546578df8e8b52dba1cf68cc9863b540",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "e4248617d26f1531b65f",
+    "url": "/covid-19/static/css/main.9c4f40bf.chunk.css"
+  },
+  {
+    "revision": "6f7f5f32ab418bce5b90",
+    "url": "/covid-19/static/js/2.03054070.chunk.js"
+  },
+  {
+    "revision": "1a1f58c879a212eae21472635da43157",
+    "url": "/covid-19/static/js/2.03054070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4248617d26f1531b65f",
+    "url": "/covid-19/static/js/main.1e473688.chunk.js"
+  },
+  {
+    "revision": "b66ef79868b06e360334",
+    "url": "/covid-19/static/js/runtime-main.38128e11.js"
+  }
+]);
